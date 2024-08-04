b72579f8e360428844e606a00af2b1aacb368203
mohamed-zekrallah <mohamed.zekrallah@vodafone.com>
2024-08-04 21:22:59 +0300

    test: add ui text

--- MoviesUITests/MoviesUITests.swift
+++ MoviesUITests/MoviesUITests.swift
@@ -8,34 +8,89 @@
 import XCTest
 
 final class MoviesUITests: XCTestCase {
-
+  var app = XCUIApplication()
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testMovieCellExists() throws {
+        let firstCell = app.tables.cells.firstMatch
+        XCTAssertTrue(firstCell.exists)
+        XCTAssertTrue(firstCell.staticTexts["titleLabel"].exists)
+        XCTAssertTrue(firstCell.images["posterImage"].exists)
+        XCTAssertTrue(firstCell.staticTexts["releaseDateLabel"].exists)
+        XCTAssertTrue(firstCell.staticTexts["scoreLabel"].exists)
+        XCTAssertTrue(firstCell.images["scoreIcon"].exists)
     }
+    
+    func testTabBarTabsExistsWithCorrentName(){
+        let tabs = app.tabBars.buttons
+        let firstTab = tabs.element(boundBy: 0)
+        let secondTab = tabs.element(boundBy: 1)
+        let thidTab = tabs.element(boundBy: 2)
+        
+        XCTAssertEqual(firstTab.label, "Trending")
+        XCTAssertEqual(secondTab.label, "Now playing")
+        XCTAssertEqual(thidTab.label, "Upcoming")
+    }
+    
+    func testTabBarTabsNavigation(){
+        let tabs = app.tabBars.buttons
+        let firstTab = tabs.element(boundBy: 0)
+        let secondTab = tabs.element(boundBy: 1)
+        let thidTab = tabs.element(boundBy: 2)
+        
+        let navigationBar = app.navigationBars.element
+        
+        firstTab.tap()
+        XCTAssertTrue(navigationBar.staticTexts["Trending"].exists)
+        secondTab.tap()
+        XCTAssertTrue(navigationBar.staticTexts["Now playing"].exists)
+        thidTab.tap()
+        XCTAssertTrue(navigationBar.staticTexts["Upcoming"].exists)
+    }
+  
+    func testNavigateToDetailsScreen(){
+        app.tables.cells.firstMatch.tap()
+        let detailsView = app.otherElements["movieDetailsScreen"]
+        XCTAssertTrue(detailsView.staticTexts["detailsGenresLabel"].exists)
+        XCTAssertTrue(detailsView.textViews["detailsOverviewTextView"].exists)
+        XCTAssertTrue(detailsView.images["detailsPosterImage"].exists)
+        let tagsCollection = detailsView.collectionViews["tagsCollectionView"]
+        XCTAssertTrue(tagsCollection.exists)
+        XCTAssertEqual(tagsCollection.cells.count, 4)
+    }
+    
+    func testSearch(){
+        // activate search
+        let table = app.tables.firstMatch
+        let searchField = app.searchFields.element
+        searchField.tap()
+        // get third cell title
+        let thirdCellTitle = table.cells.element(boundBy: 3).staticTexts["titleLabel"].label
+        // search for third cell
+        searchField.typeText(thirdCellTitle)
+        app.keyboards.firstMatch.buttons["Search"].firstMatch.tap()
+        //first search result now should be same as what we have typed
+        let firstCellTitle = table.cells.firstMatch.staticTexts["titleLabel"].label
+        XCTAssertEqual(firstCellTitle, thirdCellTitle)
+        app.tables.cells.firstMatch.tap()
+        
+    }
+    
+    func testFilters(){
+        //select action filter
+        let filters = app.collectionViews.firstMatch
+        let actionFilter = filters.cells.staticTexts["Action"]
+        actionFilter.tap()
+        //navigate to first movie
+        app.tables.cells.firstMatch.tap()
+        //genres should contain "Action" word
+        let detailsView = app.otherElements["movieDetailsScreen"]
+        XCTAssertTrue(detailsView.staticTexts["detailsGenresLabel"].label.contains("Action"))
+    }
+    
 }
+
